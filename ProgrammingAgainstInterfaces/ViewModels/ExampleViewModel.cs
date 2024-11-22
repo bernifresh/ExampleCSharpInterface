40cefa0c1f86b988c1b7ec976eb4fb604c2c6973
Bernard Largin <bernard-largin@web.de>
2024-11-22 17:39:34 +0100

    [chore] Created implementation of example view model interface

---
+++ ProgrammingAgainstInterfaces/ViewModels/ExampleViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.ObjectModel;
+using ProgrammingAgainstInterfaces.ViewModels.Interfaces;
+
+namespace ProgrammingAgainstInterfaces.ViewModels;
+
+public class ExampleViewModel : IExampleViewModel
+{
+    public string Title { get; set; }
+    public ObservableCollection<string> Names { get; }
+    public double RandomNumber { get; }
+
+    internal ExampleViewModel()
+    {
+        Title = "Example ViewModel";
+        Names = ["Hello, World!", "Test ViewModel!", "Bernard ViewModel!", "Olaf ViewModel!", "Ferdinant"]; 
+        RandomNumber = Random.Shared.NextDouble();
+    }
+}
