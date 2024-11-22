ddbaf3852ec3957886387807dc787a7d49827dc0
Bernard Largin <bernard-largin@web.de>
2024-11-22 17:40:41 +0100

    [chore] Set design instance and user control in main window
    - added example user control
    - set data context behind code in main window

--- ProgrammingAgainstInterfaces/MainWindow.xaml.cs
+++ ProgrammingAgainstInterfaces/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using ProgrammingAgainstInterfaces.ViewModels;
 
 namespace ProgrammingAgainstInterfaces;
 
@@ -19,5 +11,7 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        DataContext = new ExampleViewModel();
+        WindowState = WindowState.Maximized;
     }
 }
