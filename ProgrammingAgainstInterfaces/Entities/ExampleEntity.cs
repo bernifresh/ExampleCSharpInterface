585340541731ca43bcd08559143720618c2788b4
Bernard Largin <bernard-largin@web.de>
2024-11-22 18:38:33 +0100

    [model] Added example entity

---
+++ ProgrammingAgainstInterfaces/Entities/ExampleEntity.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ProgrammingAgainstInterfaces.Entities;
+
+public class ExampleEntity
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string Id { get; set; } = null!;
+
+    [BsonElement("title")]
+    public string Title { get; set; } = null!;
+
+    [BsonElement("names")]
+    public List<string> Names { get; set; } = null!;
+
+    [BsonElement("randomNumber")]
+    public double RandomNumber { get; set; }
+}
