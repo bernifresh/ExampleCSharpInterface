d45c73809e2a4c3d7c5f20c212f97eaf1e055dd0
Bernard Largin <bernard-largin@web.de>
2024-11-22 18:29:01 +0100

    [development] Added local mongo database with init script

---
+++ init-mongo/init.js
@@ -0,0 +1,56 @@
+﻿db = db.getSiblingDB("mydatabase");
+
+db.exampleViewModels.insertMany([
+    {
+        title: "Example ViewModel 1",
+        names: ["Hello, World!", "Test ViewModel!", "Bernard ViewModel!", "Olaf ViewModel!", "Ferdinant"],
+        randomNumber: Math.random()
+    },
+    {
+        title: "Example ViewModel 2",
+        names: ["Hello, World!", "Test ViewModel!", "Bernard ViewModel!", "Olaf ViewModel!", "Ferdinant"],
+        randomNumber: Math.random()
+    },
+    {
+        title: "Example ViewModel 3",
+        names: ["Hello, World!", "Test ViewModel!", "Bernard ViewModel!", "Olaf ViewModel!", "Ferdinant"],
+        randomNumber: Math.random()
+    },
+    {
+        title: "Example ViewModel 4",
+        names: ["Hello, World!", "Test ViewModel!", "Bernard ViewModel!", "Olaf ViewModel!", "Ferdinant"],
+        randomNumber: Math.random()
+    },
+    {
+        title: "Example ViewModel 5",
+        names: ["Hello, World!", "Test ViewModel!", "Bernard ViewModel!", "Olaf ViewModel!", "Ferdinant"],
+        randomNumber: Math.random()
+    },
+    {
+        title: "Example ViewModel 6",
+        names: ["Hello, World!", "Test ViewModel!", "Bernard ViewModel!", "Olaf ViewModel!", "Ferdinant"],
+        randomNumber: Math.random()
+    },
+    {
+        title: "Example ViewModel 7",
+        names: ["Hello, World!", "Test ViewModel!", "Bernard ViewModel!", "Olaf ViewModel!", "Ferdinant"],
+        randomNumber: Math.random()
+    },
+    {
+        title: "Example ViewModel 8",
+        names: ["Hello, World!", "Test ViewModel!", "Bernard ViewModel!", "Olaf ViewModel!", "Ferdinant"],
+        randomNumber: Math.random()
+    },
+    {
+        title: "Example ViewModel 9",
+        names: ["Hello, World!", "Test ViewModel!", "Bernard ViewModel!", "Olaf ViewModel!", "Ferdinant"],
+        randomNumber: Math.random()
+    },
+    {
+        title: "Example ViewModel 10",
+        names: ["Hello, World!", "Test ViewModel!", "Bernard ViewModel!", "Olaf ViewModel!", "Ferdinant"],
+        randomNumber: Math.random()
+    }
+]);
+
+print("Initialisierung abgeschlossen: Beispiel-Daten für exampleViewModels wurden eingefügt.");
