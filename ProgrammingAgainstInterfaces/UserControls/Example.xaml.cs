b20f5f1c2d1835a47384d861c8315b2f176a4c1e
Bernard Largin <bernard-largin@web.de>
2024-11-22 17:21:21 +0100

    [chore] Create example user control
    - added data context example view model interface
    - added label to show title
    - added random number for entertaining
    - added list view to display names

---
+++ ProgrammingAgainstInterfaces/UserControls/Example.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace ProgrammingAgainstInterfaces.UserControls;
+
+public partial class Example : UserControl
+{
+    public Example()
+    {
+        InitializeComponent();
+    }
+}
