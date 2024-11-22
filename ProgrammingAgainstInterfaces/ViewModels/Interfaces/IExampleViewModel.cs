7f2c7c0b4af56095f9339d55e6232be70b42de11
Bernard Largin <bernard-largin@web.de>
2024-11-22 17:11:20 +0100

    [chore] Added interface for example component data context
    - added a view model interface

---
+++ ProgrammingAgainstInterfaces/ViewModels/Interfaces/IExampleViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.ObjectModel;
+
+namespace ProgrammingAgainstInterfaces.ViewModels.Interfaces;
+
+public interface IExampleViewModel
+{
+    public string Title { get; set; }
+    public ObservableCollection<string> Names { get; }
+    public double RandomNumber { get; }
+}
