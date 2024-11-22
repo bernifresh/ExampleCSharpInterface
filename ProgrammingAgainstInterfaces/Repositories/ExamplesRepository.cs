fb24d0e489270517d93433e0b49f958cacf3b297
Bernard Largin <bernard-largin@web.de>
2024-11-22 18:39:12 +0100

    [chore] Created crud repository for example mongo db

---
+++ ProgrammingAgainstInterfaces/Repositories/ExamplesRepository.cs
@@ -0,0 +1,35 @@
+﻿using MongoDB.Driver;
+using ProgrammingAgainstInterfaces.Entities;
+using ProgrammingAgainstInterfaces.Repositories.Interfaces;
+
+namespace ProgrammingAgainstInterfaces.Repositories;
+
+public class ExampleRepository(IMongoDatabase database) : IExampleRepository
+{
+    private readonly IMongoCollection<ExampleEntity> _collection = database.GetCollection<ExampleEntity>("exampleViewModels");
+
+    public async Task<IEnumerable<ExampleEntity>> GetAllAsync()
+    {
+        return await _collection.Find(_ => true).ToListAsync();
+    }
+
+    public async Task<ExampleEntity> GetByIdAsync(string id)
+    {
+        return await _collection.Find(entity => entity.Id == id).FirstOrDefaultAsync();
+    }
+
+    public async Task AddAsync(ExampleEntity entity)
+    {
+        await _collection.InsertOneAsync(entity);
+    }
+
+    public async Task UpdateAsync(string id, ExampleEntity entity)
+    {
+        await _collection.ReplaceOneAsync(e => e.Id == id, entity);
+    }
+
+    public async Task DeleteAsync(string id)
+    {
+        await _collection.DeleteOneAsync(e => e.Id == id);
+    }
+}
---
+++ ProgrammingAgainstInterfaces/Repositories/Interfaces/IExampleRepository.cs
@@ -0,0 +1,12 @@
+﻿using ProgrammingAgainstInterfaces.Entities;
+
+namespace ProgrammingAgainstInterfaces.Repositories.Interfaces;
+
+public interface IExampleRepository
+{
+    Task<IEnumerable<ExampleEntity>> GetAllAsync();
+    Task<ExampleEntity> GetByIdAsync(string id);
+    Task AddAsync(ExampleEntity entity);
+    Task UpdateAsync(string id, ExampleEntity entity);
+    Task DeleteAsync(string id);
+}
